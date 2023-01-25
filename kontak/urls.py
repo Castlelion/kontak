650a5d74f06628035ecda07e83abff461f538533
Castle <aldis.a101@gmail.com>
2023-01-25 16:37:44 +0700

    home baru

--- kontak/urls.py
+++ kontak/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from kontak import views
+
 # from django.conf import settings
 # from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('siswa.urls'), name='home'),
+    path('', views.Home),
+    path('siswa/', include('siswa.urls')),
     path('test/', include('sekolah.urls'), name='test'),
 ]
 
